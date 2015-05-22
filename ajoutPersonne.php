bcadad80b982d6c16fb45a0ae7bf2d4ba8da3395
tamicasireim <msimatic.utc@gmail.com>
2015-05-22 10:00:56 +0200

    ajoutPersonne presque fonctionnel + modif gitignore
    le gitignore ne prend plus le fichier connect.php, vu que ce dernier contient des informations locales pour chacunes de nos copies du répertoire.
    ajoutPersonne est presque fonctionnel, il le sera vraiment ce soir :)

--- ajoutPersonne.php
+++ ajoutPersonne.php
@@ -1,5 +1,5 @@
 <?php
-$titre = "Ajout d'une Personne" ;
+$title = "Ajout d'une Personne" ;
 include 'includes/header.php' ;
 
 if(!isset($_POST['nom']) && !isset($_POST['prenom'])){
@@ -7,12 +7,12 @@ if(!isset($_POST['nom']) && !isset($_POST['prenom'])){
 } else {
 	$erreur = verifier_arguments(['nom','prenom']);
 
-	if(!isset($_POST['is_karateka'])){
+	if( isset($_POST['is_karateka']) ){
 		$erreur.= verifier_arguments(['photo','poids','taille','ceinture']);
 		if($_POST['ceinture'] == 'noire'){
 			$erreur.= verifier_arguments(['dans']);
 		}
-	} elseif ($_POST['is_dirigeant'] != NULL) {
+	} elseif ( isset($_POST['is_dirigeant']) ) {
 		$erreur .= verifier_arguments(['email','tel']) ;
 	} else {
 		$erreur .= 'Une Personne ne peut pas être ni dirigeante ni Karatéka';
@@ -23,7 +23,17 @@ if(!isset($_POST['nom']) && !isset($_POST['prenom'])){
 		include 'includes/Formulaires/ajoutKarateka.php';
 	} else {
 		echo "JOUA ET ALLEGRESSE";
-		// Requêtes SQL à faire
+		$vconnect = Connect();
+		$request = 'INSERT INTO person (firstname, lastname)
+					VALUES (\''.$_POST['prenom'].'\', \''.$_POST['nom'].'\');' ;
+		if( pg_query($vconnect, $request) == false ) {
+			echo "erreur du con" ;
+			exit();
+		}
+
+		if ( isset($POST['is_dirigeant']) ) {
+
+		}
 	}
 
 }
