0eb2935361abb8ac20a559bcff217e5417c51f9a
tamicasireim <msimatic.utc@gmail.com>
2015-06-16 03:43:55 +0200

    intégration de la page au site

--- public-html/viewCompetitionAll.php
+++ public-html/viewCompetitionAll.php
@@ -18,7 +18,7 @@
 	<table class="table table-bordered">
 	<CAPTION>Liste des competitions</CAPTION>
 	<thead>
-	<tr><th>#</th><th>Name</th><th>place</th><th>date</th><th>type</th><th>organisator</th><th>inscrit</th><th>statut</th><th>Details</th></tr></thead><tbody>
+	<tr><th>#</th><th>Name</th><th>place</th><th>date</th><th>type</th><th>organisator</th><th>inscrit</th><th>statut</th><th>Details</th><th>Results</th></tr></thead><tbody>
 	<?php 
 	while($competition = pg_fetch_array($resultcompetition)){
 			$requestnbkarateka = "SELECT count(*) FROM participate WHERE $competition[id] = participate.competition;";
@@ -30,7 +30,15 @@
 
 		
 	echo"<tr>";
-		echo"<td>$competition[id]</td><td>$competition[name]</td><td>$competition[place]</td><td>$competition[date]</td><td>$competition[type]</td><td>$competition[organisator]</td><td>$result[0] / 8</td><td>$competition[statut]</td><td><a href='viewCompetition.php?id=$competition[id]'>here</a></td>";
+		echo"<td>$competition[id]</td><td>$competition[name]</td>
+			<td>$competition[place]</td>
+			<td>$competition[date]</td>
+			<td>$competition[type]</td>
+			<td>$competition[organisator]</td>
+			<td>$result[0] / 8</td>
+			<td>$competition[statut]</td>
+			<td><a href='viewCompetition.php?id=$competition[id]'>here</a></td>
+			<td><a href='viewConfrontation.php?id=$competition[id]'>here</a></td>";
 		echo "</tr>";
 	}?>
 		</tbody></table></div></div></div></div></section>
